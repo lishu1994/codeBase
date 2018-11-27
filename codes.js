5fe1cd50f6f39fb9cae48812210aec05f95c3b1c
lishu <lishu1994930@163.com>
2018-11-27 09:49:48 +0800

    mapKeys

---
+++ codes.js
@@ -0,0 +1,25 @@
+/**
+ * 根据map规则将target中的项修改为source中的值
+ * example:
+ * map = {
+ *   data: 'data',
+ *   props: 'properties'
+ * }
+ * source = {
+ *   data: 1,
+ *   props: 2
+ * }
+ * target = {
+ *   data: 3,
+ *   properties: 4
+ * }
+ * 
+ */
+
+function mapKeys(source, target, map) {
+  Object.keys(map).forEach(function (key) {
+    if (source[key]) {
+      target[map[key]] = source[key];
+    }
+  });
+}
